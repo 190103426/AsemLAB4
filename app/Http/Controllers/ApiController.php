56bdcb5877c20c682323825d8a489bd0d372be62
190103425 <190103426@stu.sdu.edu.kz>
2021-03-16 03:14:52 +0600

    ApiController

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+
+class ApiController extends Controller
+{
+    public function index(Request $request){
+        $post = post::all();
+
+        return response($post, 200);
+
+    }
+
+    public function get_post(Request $request){
+        $post = post::find($request -> post_id);
+
+        if($post == null){
+        return response(['message' => 'There is no post'],404);
+        }
+
+        return response($post, 200);
+
+    }
+}
